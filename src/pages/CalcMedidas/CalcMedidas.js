62c912d8ac2bc30616cfe01e665141f56c9e91d2
Eduardo Lovo <eduardo.llovo@gmail.com>
2023-07-05 16:55:14 -0300

    calc virol e lençol

--- src/pages/CalcMedidas/CalcMedidas.js
+++ src/pages/CalcMedidas/CalcMedidas.js
@@ -12,49 +12,59 @@ export const CalcMedidas = () => {
 
         const lar = +e.target.largura.value
         const comp = +e.target.comprimento.value
+        const acessorio = e.target.acessorio.value
+
 
         //Externo
         const exlar = lar + 2 
-        const excomp = comp + 2
-        var externo = ('ex:', exlar, 'x', excomp) 
-        console.log(externo);
+        const excomp = comp + 2        
 
         //Interno
         const inlar = exlar - 14
         const incomp = excomp - 14
-        console.log('in:', inlar, 'x', incomp);
 
         //Colcão
         const collar = inlar - 3
         const colcomp = incomp - 3
-        console.log('colchão:', collar, 'x', colcomp);
 
-        //Lençol
-        const lenlar = collar + 40
-        const lencomp = colcomp + 40
-        console.log('lençol:', lenlar, 'x', lencomp);
+        const lenlar = collar + 40        
+        
+        const lencomp = () => {
+            if (acessorio === 'lencol') {
+                const lencomp = colcomp + 40
+                return lencomp
+            } else {
+                const lencomp = colcomp + 70
+                return lencomp
+            }
+        }
+       
 
         setResultadoPhant(<div className='fs-5 text'>
             <div>
                 <label>Externo: </label>
-                <span>{exlar} x {excomp}</span>
+                <span> {exlar} x {excomp}</span>
             </div>
             <div className='mt-2'>
                 <label>Interno: </label>
-                <span>{inlar} x {incomp}</span>
+                <span> {inlar} x {incomp}</span>
             </div>
             <div className='mt-2'>
                 <label>Colchão: </label>
-                <span>{collar} x {colcomp}</span>
+                <span> {collar} x {colcomp}</span>
             </div>
             <div className='mt-2'>
-                <label>Lençol: </label>
-                <span>{lenlar} x {lencomp}</span>
+                {acessorio === 'lencol'? <label>Lençol: </label>: <label>Virol:</label>}
+                
+                <span> {lenlar} x {lencomp()}</span>
             </div>
+            {acessorio === 'lencol'? 
             <div>
                 <label>Quadrado: </label>
                 <span> Padrão</span>
             </div>
+            : ''
+            }
         </div>)
     }
 
@@ -64,33 +74,35 @@ export const CalcMedidas = () => {
         const lar = +e.target.largura.value
         const comp = +e.target.comprimento.value
         const alt = +e.target.altura.value
+        const acessorio = e.target.acessorio.value
+
+        //Interno
+        const inlar = lar + 3
+        const incomp = comp + 3
 
         //Externo
-        const exlar = lar + 2 
-        const excomp = comp + 2
-        var externo = ('ex:', exlar, 'x', excomp) 
-        console.log(externo);
+        const exlar = inlar + 14
+        const excomp = incomp + 14
 
         //Altura 
-        const dif = alt + 11
+        const dif = alt - 10
         const exalt = dif + 23
         const inalt = dif + 21
 
-
-        //Interno
-        const inlar = exlar - 14
-        const incomp = excomp - 14
-        console.log('in:', inlar, 'x', incomp);
-
-        //Colcão
-        const collar = inlar - 3
-        const colcomp = incomp - 3
-        console.log('colchão:', collar, 'x', colcomp);
-
         //Lençol
-        const lenlar = collar + 40
-        const lencomp = colcomp + 40
-        console.log('lençol:', lenlar, 'x', lencomp);
+        const lenlar = lar + 40
+
+        const lencomp = () => {
+            if (acessorio === 'lencol') {
+                const lencomp = comp + 40
+                return lencomp
+            } else {
+                const lencomp = comp + 70
+                return lencomp
+            }
+        }
+
+        // const lencomp = colcomp + 40
 
         setResultado(
         <div className='fs-5 text'>
@@ -105,17 +117,27 @@ export const CalcMedidas = () => {
             </div>
 
             <div className='mt-2'>
-                <label>Lençol: </label>
-                <span> {lenlar} x {lencomp}</span>
+                {acessorio === 'lencol'? <label>Lençol: </label>: <label>Virol:</label>}
+                
+                <span> {lenlar} x {lencomp()}</span>
             </div>
+            {acessorio === 'lencol'? 
             <div>
                 <label>Quadrado: </label>
                 <span> {alt + 10} x {alt + 10}</span>
             </div>
+            : ''
+            }
+            {acessorio === 'virolLiso'?
+            <div>
+                <label>Retangulo: </label>
+                <span> {lenlar} x 50</span>
+            </div>: ''}
         </div>
         )
 
     }
+
   return (
     <div>
         <div className='p-5 glass2'>
@@ -130,6 +152,14 @@ export const CalcMedidas = () => {
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Comprimento</label>
                             <input type="number" class="form-control" id="comprimento"/>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="acessorio" value='lencol' name="radio-stacked" />
+                            <label class="form-check-label" for="acessorio">Lençol</label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input type="radio" class="form-check-input" id="acessorio" value='viral' name="radio-stacked" />
+                            <label class="form-check-label" for="acessorio">Virol</label>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
@@ -156,6 +186,18 @@ export const CalcMedidas = () => {
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Altura</label>
                             <input type="number" class="form-control" id="altura"/>
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="acessorio" value='lencol' name="radio-stacked" />
+                            <label class="form-check-label" for="acessorio">Lençol</label>
+                        </div>
+                        <div class="form-check ">
+                            <input type="radio" class="form-check-input" id="acessorio" value='virolLiso' name="radio-stacked" />
+                            <label class="form-check-label" for="acessorio">Virol Liso</label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input type="radio" class="form-check-input" id="acessorio" value='virol' name="radio-stacked" />
+                            <label class="form-check-label" for="acessorio">Virol com Aplique</label>
                         </div>
             
                         <button type="submit" class="btn btn-primary">Submit</button>
