63cbcad941c60796ea246fc2569d7bd72c9ffd71
EduardoLovo <eduardo.llovo@gmail.com>
2024-12-06 11:59:34 -0300

    Atualizado pagina create lençol pronta entrega

--- src/pages/CatalogoLencolPE/CatalogoProntaEntrega.js
+++ src/pages/CatalogoLencolPE/CatalogoProntaEntrega.js
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
-import { ListaDeLoncolProntaEntrega } from "../../components/ListaDeLoncolProntaEntrega/ListaDeLoncolProntaEntrega";
+import React, { useState } from 'react';
+import { ListaDeLoncolProntaEntrega } from '../../components/ListaDeLoncolProntaEntrega/ListaDeLoncolProntaEntrega';
+import { Link } from 'react-router-dom';
 
 export const CatalogoProntaEntrega = () => {
-  const [tamanho, setTamanho] = useState("");
-  return (
-    <div>
-      <div className="menuDoCatalogoSintetico">
-        <button onClick={() => setTamanho("Berço")}>Berço</button>
-        <button onClick={() => setTamanho("Junior")}>Junior</button>
-        <button onClick={() => setTamanho("Solteiro")}>Solteiro</button>
-        <button onClick={() => setTamanho("Solteirao")}>Solteirão</button>
-        <button onClick={() => setTamanho("Viuva")}>Viuva</button>
-        <button onClick={() => setTamanho("Casal")}>Casal</button>
-        <button onClick={() => setTamanho("Queen")}>Queen</button>
-        <button onClick={() => setTamanho("King")}>King</button>
-      </div>
-      <div>
-        <ListaDeLoncolProntaEntrega tamanho={tamanho} />
-      </div>
-    </div>
-  );
+    const [tamanho, setTamanho] = useState('');
+
+    const type = localStorage.getItem('user');
+    return (
+        <div>
+            <div className="menuDoCatalogoSintetico">
+                <button onClick={() => setTamanho('Berço')}>Berço</button>
+                <button onClick={() => setTamanho('Junior')}>Junior</button>
+                <button onClick={() => setTamanho('Solteiro')}>Solteiro</button>
+                <button onClick={() => setTamanho('Solteirão')}>
+                    Solteirão
+                </button>
+                <button onClick={() => setTamanho('Viuva')}>Viuva</button>
+                <button onClick={() => setTamanho('Casal')}>Casal</button>
+                <button onClick={() => setTamanho('Queen')}>Queen</button>
+                <button onClick={() => setTamanho('King')}>King</button>
+                {type === 'adm' && (
+                    <button>
+                        <Link to={'/lencol-create'}>Adicionar </Link>
+                    </button>
+                )}
+            </div>
+            <div>
+                <ListaDeLoncolProntaEntrega tamanho={tamanho} />
+            </div>
+        </div>
+    );
 };
--- src/pages/CatalogoLencolPE/LencolCreate.js
+++ src/pages/CatalogoLencolPE/LencolCreate.js
@@ -1,142 +1,159 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Api } from "../../Api/Api";
-import { toast } from "react-toastify";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Api } from '../../Api/Api';
+import { toast } from 'react-toastify';
 
 export const LencolCreate = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const number = e.target.number.value;
-    const img = e.target.img.value;
-    const quantidade = e.target.quantidade.value;
-    const tamanho = e.target.tamanho.value;
-    const cor = e.target.cor.value;
+        const number = e.target.number.value;
+        const img = e.target.img.value;
+        const quantidade = e.target.quantidade.value;
+        const tamanho = e.target.tamanho.value;
+        const cor = e.target.cor.value;
 
-    const payload = {
-      number,
-      img,
-      quantidade,
-      tamanho,
-      cor,
+        const payload = {
+            number,
+            img,
+            quantidade,
+            tamanho,
+            cor,
+        };
+
+        const response = await Api.buildApiPostRequest(
+            Api.addLencolUrl(),
+            payload,
+            true
+        );
+
+        if (response.status === 200) {
+            // Product created successfully
+            toast.success('Lençol adicionado com sucesso!');
+            navigate('/catalogo-lencol-pronta-entrega');
+        } else {
+            // Error
+        }
     };
 
-    const response = await Api.buildApiPostRequest(
-      Api.addLencolUrl(),
-      payload,
-      true
-    );
+    return (
+        <div>
+            <div className="cabecalho">
+                <h1>Adicionar novo Lençol</h1>
+            </div>
+            <div className="row align-items-center">
+                <div className="col">
+                    <form
+                        className="d-flex flex-column align-items-center"
+                        onSubmit={handleSubmit}
+                    >
+                        <h3>Editar</h3>
+                        <div className="input-group mb-3 w-50">
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Numero
+                            </span>
+                            <input
+                                id="number"
+                                name="number"
+                                type="text"
+                                className="form-control"
+                                aria-label="Sizing example input"
+                                aria-describedby="inputGroup-sizing-default"
+                            />
+                        </div>
+                        <div className="input-group mb-3 w-50">
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Imagem
+                            </span>
+                            <input
+                                id="img"
+                                name="img"
+                                type="text"
+                                className="form-control"
+                                aria-label="Sizing example input"
+                                aria-describedby="inputGroup-sizing-default"
+                            />
+                        </div>
+                        <div className="input-group mb-3 w-50">
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Quantidade
+                            </span>
+                            <input
+                                id="quantidade"
+                                name="quantidade"
+                                type="text"
+                                className="form-control"
+                                aria-label="Sizing example input"
+                                aria-describedby="inputGroup-sizing-default"
+                            />
+                        </div>
+                        <div className="input-group mb-3 w-50">
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Tamanho
+                            </span>
+                            <select
+                                id="tamanho"
+                                name="tamanho"
+                                className="form-control"
+                                placeholder="tamanho"
+                            >
+                                <option></option>
+                                <option>Berço</option>
+                                <option>Junior</option>
+                                <option>Solteiro</option>
+                                <option>Solteirão</option>
+                                <option>Viuva</option>
+                                <option>Casal</option>
+                                <option>Queen</option>
+                                <option>King</option>
+                            </select>
+                        </div>
 
-    if (response.status === 200) {
-      // Product created successfully
-      toast.success("Lençol adicionado com sucesso!");
-      navigate("/catalogo-lencol-pronta-entrega");
-    } else {
-      // Error
-    }
-  };
+                        <div className="input-group mb-3 w-50">
+                            <span
+                                className="input-group-text"
+                                id="inputGroup-sizing-default"
+                            >
+                                Cor
+                            </span>
+                            <select
+                                id="cor"
+                                name="cor"
+                                className="form-control"
+                                placeholder="cor"
+                            >
+                                <option></option>
+                                <option>Bege</option>
+                                <option>Azul</option>
+                                <option>Branco</option>
+                                <option>Rosa</option>
+                                <option>Rosa Bebe</option>
+                                <option>Palha</option>
+                                <option>Cinza</option>
+                                <option>Prata</option>
+                            </select>
+                        </div>
 
-  return (
-    <div>
-      <h1>adionar novo lençol</h1>
-      <div className="row align-items-center">
-        <div className="col">
-          <form
-            className="d-flex flex-column align-items-center"
-            onSubmit={handleSubmit}
-          >
-            <h3>Editar</h3>
-            <div className="input-group mb-3 w-50">
-              <span className="input-group-text" id="inputGroup-sizing-default">
-                Numero
-              </span>
-              <input
-                id="number"
-                name="number"
-                type="text"
-                className="form-control"
-                aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default"
-              />
+                        <button className="btnPadrao" type="submit">
+                            Adicionar
+                        </button>
+                    </form>
+                </div>
             </div>
-            <div className="input-group mb-3 w-50">
-              <span className="input-group-text" id="inputGroup-sizing-default">
-                Imagem
-              </span>
-              <input
-                id="img"
-                name="img"
-                type="text"
-                className="form-control"
-                aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default"
-              />
-            </div>
-            <div className="input-group mb-3 w-50">
-              <span className="input-group-text" id="inputGroup-sizing-default">
-                Quantidade
-              </span>
-              <input
-                id="quantidade"
-                name="quantidade"
-                type="text"
-                className="form-control"
-                aria-label="Sizing example input"
-                aria-describedby="inputGroup-sizing-default"
-              />
-            </div>
-            <div className="input-group mb-3 w-50">
-              <span className="input-group-text" id="inputGroup-sizing-default">
-                Tamanho
-              </span>
-              <select
-                id="tamanho"
-                name="tamanho"
-                className="form-control"
-                placeholder="tamanho"
-              >
-                <option></option>
-                <option>Berço</option>
-                <option>Junior</option>
-                <option>Solteiro</option>
-                <option>Solteiro / Solteirão</option>
-                <option>Viuva</option>
-                <option>Casal</option>
-                <option>Queen</option>
-                <option>King</option>
-              </select>
-            </div>
-
-            <div className="input-group mb-3 w-50">
-              <span className="input-group-text" id="inputGroup-sizing-default">
-                Cor
-              </span>
-              <select
-                id="cor"
-                name="cor"
-                className="form-control"
-                placeholder="cor"
-              >
-                <option></option>
-                <option>Bege</option>
-                <option>Azul</option>
-                <option>Branco</option>
-                <option>Rosa</option>
-                <option>Rosa Bebe</option>
-                <option>Palha</option>
-                <option>Cinza</option>
-                <option>Prata</option>
-              </select>
-            </div>
-
-            <button className="btnPadrao" type="submit">
-              Adicionar
-            </button>
-          </form>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
